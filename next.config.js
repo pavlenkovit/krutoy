c07223506b18378d1a8cad12d310c4f6c10f45d7
pavlenkovit <vitpvln@gmail.com>
2019-09-30 18:03:48 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const withImages = require('next-images');
 const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
 
-module.exports = withImages(withCss(withSass({
+module.exports = withImages(withSass({
   target: 'serverless',
-  //useFileSystemPublicRoutes: false,
-  pageExtensions: ['jsx', 'js'],
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[hash:base64:5]__[local]",
   },
-})));
+}));
