5bda15f054e32cdef2800e1dc4a9967e7cdad264
pavlenkovit <vitpvln@gmail.com>
2019-10-01 00:07:04 +0300

    fic

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.prepare()
       return handle(req, res)
     });
 
-    server.listen(3000, err => {
+    server.listen(3001, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3001');
     })
